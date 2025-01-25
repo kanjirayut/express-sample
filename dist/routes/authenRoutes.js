b8f235bf52bc286a44995e0d66c1ee2f154b66ed
jirayut <>
2025-01-25 19:48:30 +0700

    fist-project

--- dist/routes/authenRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authenController_1 = require("../controllers/authenController");
-const router = (0, express_1.Router)();
-// เส้นทางสำหรับผู้ใช้
-router.post("/login", authenController_1.login);
-router.get("/logout", authenController_1.logout);
-exports.default = router;
--- dist/routes/profileRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const profileController_1 = require("../controllers/profileController");
-const middlewares_1 = require("../middlewares");
-const router = (0, express_1.Router)();
-// เส้นทางสำหรับผู้ใช้
-router.get("/", middlewares_1.authMiddleware, profileController_1.getProfile);
-exports.default = router;
